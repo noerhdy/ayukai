8839185f17b4d807a1348aff5427d25456ff7f22
noerhdy <hidayatnoer11@gmail.com>
2024-07-05 02:46:04 +0700

    am/pm to dark mode

--- src/components/Fragments/ModalProfil.jsx
+++ src/components/Fragments/ModalProfil.jsx
@@ -11,23 +11,24 @@ const ModalProfil = ({ isVisible, onClose }) => {
     <AnimatePresence>
       {isVisible && (
         <motion.div
-          className="overflow-y-auto sm:w-3/5 sm:h-5/6 h-full text-wrap :top-0 z-50 space-y-2 dark:text-black bg-zinc-950 dark:bg-zinc-100 text-zinc-300 absolute sm:bottom-0 sm:right-0 p-8"
+          className="overflow-y-auto sm:w-3/5 sm:h-5/6 h-full text-wrap :top-0 z-20 space-y-2 dark:text-black bg-zinc-950 dark:bg-zinc-100 text-zinc-300 absolute sm:bottom-0 sm:right-0 p-8"
           initial={{ y: 800, rotate: 45 }}
           animate={{ y: 0, rotate: 0 }}
           exit={{ y: 800, rotate: 45 }}
           transition={{ duration: 1, ease: "easeInOut" }}
         >
           <div className="flex group relative justify-center max-w-screen-sm items-center">
+            <div className="w-full h-full bg-black opacity-60 group-hover:opacity-0 ease-in-out z-30 duration-500 absolute"></div>
             <img
               className="object-cover w-full grayscale group-hover:grayscale-0 delay-200 aspect-video bg-center duration-500 ease-in-out "
               src="./byme.jpg"
               alt="example"
             />
-            <div className=" absolute text-sm font-medium top-5 flex justify-between w-full tracking-wider px-4 text-md group-hover:text-zinc-200 delay-200 duration-500 ease-in-out drop-shadow text-red-500">
+            <div className=" absolute z-40 text-sm font-medium top-5 flex justify-between w-full tracking-wider px-4 text-md group-hover:text-zinc-200 delay-200 duration-500 ease-in-out drop-shadow text-red-500">
               <h1>name? </h1>
               <h1>my name's</h1>
             </div>
-            <div className="text-4xl flex justify-center w-full absolute group-hover:text-zinc-200 delay-200 duration-500 ease-in-out drop-shadow text-red-500 font-bold">
+            <div className="text-4xl flex justify-center z-40 w-full absolute group-hover:text-zinc-200 delay-200 duration-500 ease-in-out drop-shadow text-red-500 font-bold">
               <Typewriter
                 options={{
                   strings: ["unknown", "blurryface"],
@@ -36,7 +37,7 @@ const ModalProfil = ({ isVisible, onClose }) => {
                 }}
               />
             </div>
-            <div className=" absolute font-medium bottom-5 text-sm flex justify-between w-full tracking-wider px-4 text-md group-hover:text-zinc-200 delay-200 duration-500 ease-in-out drop-shadow text-red-500">
+            <div className=" absolute z-40 font-medium bottom-5 text-sm flex justify-between w-full tracking-wider px-4 text-md group-hover:text-zinc-200 delay-200 duration-500 ease-in-out drop-shadow text-red-500">
               <h1>and i don't care</h1>
               <h1>what you think</h1>
             </div>
@@ -62,7 +63,7 @@ const ModalProfil = ({ isVisible, onClose }) => {
                 </span>
                 <br /> This name is inspired by a theme in code editor called{" "}
                 <span
-                  className="underline cursor-pointer"
+                  className="underline cursor-pointer hover:text-red-500 ease-in-out duration-500 delay-100"
                   onClick={() =>
                     handleClick(
                       "https://marketplace.visualstudio.com/items?itemName=GulajavaMinistudio.mayukaithemevsc"
@@ -75,7 +76,7 @@ const ModalProfil = ({ isVisible, onClose }) => {
                 chose Ayukai because <i>we</i> like the way it sounds. This
                 theme uses the base color swatch from the{" "}
                 <span
-                  className="underline cursor-pointer"
+                  className="underline cursor-pointer hover:text-red-500 ease-in-out duration-500 delay-100"
                   onClick={() =>
                     handleClick("https://github.com/ayu-theme/vscode-ayu")
                   }
